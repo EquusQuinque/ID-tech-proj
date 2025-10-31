c13f896ec6c6ca53f6479274af77027df440e08b
RonaldoVB5 <clafleur30@sidwell.edu>
2025-10-31 16:12:31 -0400

    Create java.java

---
+++ java.java
@@ -0,0 +1,23 @@
+public int velocity;
+
+public class main {
+  int motorvel = dc.motor(49nck237923x);
+  int vel = motervel.speed;
+  int slvel = new.velocity(0.8);
+  if (slvel == true) {
+  vel = 0.2;
+  }
+  
+
+  
+}
+
+
+
+public boolean velocity(int s){
+  if (s > 0.8) {
+    return true;
+  }
+  
+}
+  
