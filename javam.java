a18907fb24a0a433f497eb15ec32fe2b71edade1
RonaldoVB5 <clafleur30@sidwell.edu>
2025-11-02 14:22:25 -0500

    Update and rename java.java to javam.java

---
+++ javam.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class java {
+
+  public static void main(string[] args) {
+    int motorvelfl = dc.motor(49nck237923a);
+    int motorvelfr = dc.motor(49nck237923y);
+    int motorvelbl = dc.motor(49nck237973x);
+    int motorvelbr = dc.motor(49nck237953l);
+    int vel1 = getSpeed(motorvelfl);
+    int vel2 = getSpeed(motorvelfr);
+    int vel3 = getSpeed(motorvelfl);
+    int vel4 = getSpeed(motorvelfl);
+  
+    int slvel = new.velocity(vel1, vel2, vel3, vel4);
+    if (slvel == true) {
+      int set = new.bundle(0.2, 0.2, 0.2, 0.2);
+    } 
+
+    public boolean velocity(int s, int ss, int sss, int ssss){
+  if (s > 0.8 && ss > 0.8 && sss > 0.8 && ssss > 0.8) {
+    return true;
+  }
+public void bundle(int u, int d, int t, int c) {
+  int motorvelfl = dc.motor(49nck237923a);
+  int motorvelfr = dc.motor(49nck237923y);
+  int motorvelbl = dc.motor(49nck237973x);
+  int motorvelbr = dc.motor(49nck237953l);
+  int vel1 = getSpeed(motorvelfl);
+  int vel2 = getSpeed(motorvelfr);
+  int vel3 = getSpeed(motorvelfl);
+  int vel4 = getSpeed(motorvelfl);
+  vel1 /= u;
+  vel2 /= d;
+  vel3 /= t;
+  vel4 /= c;
+}
+    }
+
+}
+  
